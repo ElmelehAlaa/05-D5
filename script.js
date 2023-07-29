1ec08c8366b6c086dfd1dae4c0edb22f5a567c92
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-07-30 01:48:31 +0200

    05-D5 -FINITO

--- script.js
+++ script.js
@@ -3,11 +3,34 @@ const nav = document.getElementById("mynav");
 window.onscroll = function () {
   if (window.scrollY >= 400) {
     nav.classList.add("nav-colored");
-    nav.classList.remove("scroll-nav");
+
     bottone.classList.add("bottone-colored");
-    bottone.classList.remove("nav-botton");
   } else {
     nav.classList.remove("nav-colored");
     bottone.classList.remove("bottone-colored");
   }
 };
+
+const oneM = document.querySelectorAll("g[stroke-linecap");
+// console.log(oneM);
+// console.log(oneM[1].attributes.opacity.value);
+// oneM[1].attributes.opacity.value = 1;
+// console.log(oneM[1].attributes.opacity.value);
+
+const funDisap = function () {
+  const mNumber = Math.floor(Math.random() * 560);
+  for (let y = 0; y <= oneM.length; y++) {
+    oneM[mNumber].classList.remove("g[stroke-linecap");
+    oneM[mNumber].classList.add("disappear");
+  }
+};
+setInterval(funDisap, 200);
+
+const funAppear = function () {
+  const mNumber = Math.floor(Math.random() * 560);
+  for (let y = 0; y <= oneM.length; y++) {
+    oneM[mNumber].classList.add("g[stroke-linecap");
+    oneM[mNumber].classList.remove("disappear");
+  }
+};
+setInterval(funAppear, 200);
